f7fa6f6d4a36d2551c8962573a56d7ec4083c93d
Theracon <54535899+Theracon@users.noreply.github.com>
2021-04-22 16:51:44 +0100

    Update error handling for amount_paid

--- routes/hospital_authentication.js
+++ routes/hospital_authentication.js
@@ -73,106 +73,111 @@ router.get("/hospitals/:username/authenticate/:accession_number", middleware.isU
 
 // UPDATE(PUT): AUTHENTICATE A PATIENT/HOSPITALS
 router.put("/hospitals/:username/authenticate", middleware.isUserLoggedIn, middleware.isHospitalDepartmentCreated, function(req, res) {
-    User.findOne({ typeOfUser: "hospital", username: req.params.username }, function(err, hospital) {
-        if (!err) {
-            if (hospital) {
-                var accessionNumber = req.body.accession_number;
-                // Delete patient from hospital's patients array
-                hospital.hospitalDetails.patients.forEach(function(patient, index, patients) {
-                    if (patient.accession_number == accessionNumber) {
-                        patients.splice(index, 1);
-                    }
-                });
-                // Find patient in patients collection
-                Patient.findOne({ accession_number: accessionNumber }, function(err, patient) {
-                    if (err) {
-                        req.flash("error", "Oops! Something isn't quite right.");
-                        return res.redirect("back");
-                    }
-                    // Check for incorrect accession number
-                    if (!patient) {
-                        req.flash("error", "Patient not found.");
-                        return res.redirect("back");
-                    } else {
-
-                        // Find referring doctor and delete patient from referring doctor's patients array
-                        User.findOne({ _id: patient.referrer_id }, function(err, referrer) {
-                            if (err) {
-                                req.flash("error", "Oops! Something isn't quite right.");
-                                return res.redirect("back");
-                            }
-                            var index = referrer.referrerDetails.patients.findIndex(function(el) {
-                                return el.accession_number == accessionNumber;
-                            });
-                            referrer.referrerDetails.patients.splice(index, 1);
-                            // Change patient's status to "authenticated"
-                            patient.status = "authenticated";
-                            // Set patient authentication month & date
-                            patient.authentication_month = months[date.getMonth()];
-                            patient.authentication_date = date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear();
-                            // Set amount paid by patient 
-                            patient.amount_paid = parseInt(req.body.amount_paid, 10);
-                            // Calculate referrer commission
-                            if (parseInt(req.body.amount_paid, 10) < 5001) {
-                                patient.hospital_commission = 0.25 * (parseInt(req.body.amount_paid, 10));
-                                patient.referrer_commission = 0.6 * (0.25 * (parseInt(req.body.amount_paid, 10)));
-                                patient.platform_commission = 0.4 * (0.25 * (parseInt(req.body.amount_paid, 10)));
-                            } else if (parseInt(req.body.amount_paid, 10) > 5000 && parseInt(req.body.amount_paid, 10) < 20001) {
-                                patient.hospital_commission = 0.2 * (parseInt(req.body.amount_paid, 10));
-                                patient.referrer_commission = 0.6 * (0.2 * (parseInt(req.body.amount_paid, 10)));
-                                patient.platform_commission = 0.4 * (0.2 * (parseInt(req.body.amount_paid, 10)));
-                            } else if (parseInt(req.body.amount_paid, 10) > 20000 && parseInt(req.body.amount_paid, 10) < 50001) {
-                                patient.hospital_commission = 0.15 * (parseInt(req.body.amount_paid, 10));
-                                patient.referrer_commission = 0.6 * (0.15 * (parseInt(req.body.amount_paid, 10)));
-                                patient.platform_commission = 0.4 * (0.15 * (parseInt(req.body.amount_paid, 10)));
-                            } else if (parseInt(req.body.amount_paid, 10) > 50000 && parseInt(req.body.amount_paid, 10) < 100001) {
-                                patient.hospital_commission = 0.1 * (parseInt(req.body.amount_paid, 10));
-                                patient.referrer_commission = 0.6 * (0.1 * (parseInt(req.body.amount_paid, 10)));
-                                patient.platform_commission = 0.4 * (0.1 * (parseInt(req.body.amount_paid, 10)));
-                            } else if (parseInt(req.body.amount_paid, 10) > 100000) {
-                                patient.hospital_commission = 0.075 * (parseInt(req.body.amount_paid, 10));
-                                patient.referrer_commission = 0.6 * (0.075 * (parseInt(req.body.amount_paid, 10)));
-                                patient.platform_commission = 0.4 * (0.075 * (parseInt(req.body.amount_paid, 10)));
-                            }
-                            // Update referrer's commission
-                            referrer.referrerDetails.commission += patient.referrer_commission;
-                            // Save patient
-                            patient.save(function(err, patient) {
+    // Check the format of entered amount
+    if (isNaN(req.body.amount_paid)) {
+        req.flash("error", "Amount paid must be a number.");
+        return res.redirect("back");
+    } else {
+        User.findOne({ typeOfUser: "hospital", username: req.params.username }, function(err, hospital) {
+            if (!err) {
+                if (hospital) {
+                    var accessionNumber = req.body.accession_number;
+                    // Delete patient from hospital's patients array
+                    hospital.hospitalDetails.patients.forEach(function(patient, index, patients) {
+                        if (patient.accession_number == accessionNumber) {
+                            patients.splice(index, 1);
+                        }
+                    });
+                    // Find patient in patients collection
+                    Patient.findOne({ accession_number: accessionNumber }, function(err, patient) {
+                        if (err) {
+                            req.flash("error", "Oops! Something isn't quite right.");
+                            return res.redirect("back");
+                        }
+                        // Check for incorrect accession number
+                        if (!patient) {
+                            req.flash("error", "Patient not found.");
+                            return res.redirect("back");
+                        } else {
+                            // Find referring doctor and delete patient from referring doctor's patients array
+                            User.findOne({ _id: patient.referrer_id }, function(err, referrer) {
                                 if (err) {
                                     req.flash("error", "Oops! Something isn't quite right.");
                                     return res.redirect("back");
                                 }
-                                // Push patient into referring doctor's patients array
-                                referrer.referrerDetails.patients.unshift(patient);
-                                // Save referrer
-                                referrer.save(function(err, referrer) {
+                                var index = referrer.referrerDetails.patients.findIndex(function(el) {
+                                    return el.accession_number == accessionNumber;
+                                });
+                                referrer.referrerDetails.patients.splice(index, 1);
+                                // Change patient's status to "authenticated"
+                                patient.status = "authenticated";
+                                // Set patient authentication month & date
+                                patient.authentication_month = months[date.getMonth()];
+                                patient.authentication_date = date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear();
+                                // Set amount paid by patient 
+                                patient.amount_paid = parseInt(req.body.amount_paid, 10);
+                                // Calculate referrer commission
+                                if (parseInt(req.body.amount_paid, 10) < 5001) {
+                                    patient.hospital_commission = 0.25 * (parseInt(req.body.amount_paid, 10));
+                                    patient.referrer_commission = 0.6 * (0.25 * (parseInt(req.body.amount_paid, 10)));
+                                    patient.platform_commission = 0.4 * (0.25 * (parseInt(req.body.amount_paid, 10)));
+                                } else if (parseInt(req.body.amount_paid, 10) > 5000 && parseInt(req.body.amount_paid, 10) < 20001) {
+                                    patient.hospital_commission = 0.2 * (parseInt(req.body.amount_paid, 10));
+                                    patient.referrer_commission = 0.6 * (0.2 * (parseInt(req.body.amount_paid, 10)));
+                                    patient.platform_commission = 0.4 * (0.2 * (parseInt(req.body.amount_paid, 10)));
+                                } else if (parseInt(req.body.amount_paid, 10) > 20000 && parseInt(req.body.amount_paid, 10) < 50001) {
+                                    patient.hospital_commission = 0.15 * (parseInt(req.body.amount_paid, 10));
+                                    patient.referrer_commission = 0.6 * (0.15 * (parseInt(req.body.amount_paid, 10)));
+                                    patient.platform_commission = 0.4 * (0.15 * (parseInt(req.body.amount_paid, 10)));
+                                } else if (parseInt(req.body.amount_paid, 10) > 50000 && parseInt(req.body.amount_paid, 10) < 100001) {
+                                    patient.hospital_commission = 0.1 * (parseInt(req.body.amount_paid, 10));
+                                    patient.referrer_commission = 0.6 * (0.1 * (parseInt(req.body.amount_paid, 10)));
+                                    patient.platform_commission = 0.4 * (0.1 * (parseInt(req.body.amount_paid, 10)));
+                                } else if (parseInt(req.body.amount_paid, 10) > 100000) {
+                                    patient.hospital_commission = 0.075 * (parseInt(req.body.amount_paid, 10));
+                                    patient.referrer_commission = 0.6 * (0.075 * (parseInt(req.body.amount_paid, 10)));
+                                    patient.platform_commission = 0.4 * (0.075 * (parseInt(req.body.amount_paid, 10)));
+                                }
+                                // Update referrer's commission
+                                referrer.referrerDetails.commission += patient.referrer_commission;
+                                // Save patient
+                                patient.save(function(err, patient) {
                                     if (err) {
                                         req.flash("error", "Oops! Something isn't quite right.");
                                         return res.redirect("back");
                                     }
-                                    // Push patient into hospital's patients array
-                                    hospital.hospitalDetails.patients.unshift(patient);
-                                    // Save hospital
-                                    hospital.save(function(err, hospital) {
+                                    // Push patient into referring doctor's patients array
+                                    referrer.referrerDetails.patients.unshift(patient);
+                                    // Save referrer
+                                    referrer.save(function(err, referrer) {
                                         if (err) {
                                             req.flash("error", "Oops! Something isn't quite right.");
                                             return res.redirect("back");
                                         }
-                                        return res.redirect("/hospitals/" + hospital.username + "/patients");
-                                    })
+                                        // Push patient into hospital's patients array
+                                        hospital.hospitalDetails.patients.unshift(patient);
+                                        // Save hospital
+                                        hospital.save(function(err, hospital) {
+                                            if (err) {
+                                                req.flash("error", "Oops! Something isn't quite right.");
+                                                return res.redirect("back");
+                                            }
+                                            return res.redirect("/hospitals/" + hospital.username + "/patients");
+                                        })
+                                    });
                                 });
                             });
-                        });
-                    }
-                });
-                return;
+                        }
+                    });
+                    return;
+                }
+                req.flash("error", "Please login or create an account.");
+                return res.redirect("/login");
             }
-            req.flash("error", "Please login or create an account.");
-            return res.redirect("/login");
-        }
-        req.flash("error", "Oops! Something isn't quite right.")
-        res.redirect("back");
-    });
+            req.flash("error", "Oops! Something isn't quite right.")
+            res.redirect("back");
+        });
+    }
 });
 
 module.exports = router;
